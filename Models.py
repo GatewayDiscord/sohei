089181dedd67e1b5cf533dbeecefe93b6f243429
Ujjwal Kumar <katanadeveloper01@gmail.com>
2021-02-08 00:42:47 +0530

    ctf model

--- Models.py
+++ Models.py
@@ -18,3 +18,10 @@ class Users(db.Model):
     if __name__=="__main__":
         init_db()
 
+class Ctf(db.Model):
+    name=db.Column(db.String(100))
+    description=db.Column(db.String(500))
+
+    def __init__(self, name, description): 
+        self.name=name
+        self.description=description
