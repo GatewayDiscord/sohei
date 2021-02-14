0861182dfdec7e99d7ad78ca67b9ca68c902bf30
Elliot Alderson <60684641+0x0elliot@users.noreply.github.com>
2021-02-14 19:09:45 +0530

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -1,4 +1,9 @@
+import os
+import sys
+
+sys.path.insert(0, os.path.abspath("..")) # this is for me to fix a local bug. not a part of the project itself.
 from sohei import app
+
 
 if __name__=="__main__":
     app.run()
