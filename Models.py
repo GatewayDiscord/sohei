abf4b95c437c38a81a6da57be52d265f495c2711
elliot <elliot@localhost.localdomain>
2021-02-15 22:31:49 +0530

    Updated Models

--- Models.py
+++ Models.py
@@ -3,12 +3,13 @@ from sohei.main import db
 class Users(db.Model):
     __tablename__="users"
 
-    username=db.Column(db.String(100), primary_key=True)
-    password=db.Column(db.String(100))
+    userid=db.Column(db.Integer, primary_key=True)
+    username=db.Column(db.String(100), unique=True)
+    password=db.Column(db.String(100), unique=True)
     role=db.Column(db.String(100))
     registered_on=db.Column(db.String(100))
 
-    def __init__(self, username, password,registered_on, role):
+    def __init__(self,username, password,registered_on, role):
         self.username=username
         self.password=password
         self.registered_on=registered_on
@@ -21,7 +22,7 @@ class Users(db.Model):
 class challenges(db.Model):
     __tablename__="challenges"
 
-    challengeid=db.Column(db.String(100), primary_key=True)
+    challengeid=db.Column(db.Integer, primary_key=True)
     title=db.Column(db.String(100))
     body=db.Column(db.String(140))
     tag=db.Column(db.String(100))
@@ -43,7 +44,7 @@ class challenges(db.Model):
 class CTFs(db.Model):
     __tablename__="ctfs"
 
-    ctfid=db.Column(db.String(100), primary_key=True)
+    ctfid=db.Column(db.Integer, primary_key=True)
     ctftitle=db.Column(db.String(100))
     link=db.Column(db.String(100))
     status=db.Column(db.String(100)) #ongoing, done or upcoming
@@ -61,7 +62,7 @@ class files(db.Model):
 
     __tablename__="files"
 
-    fileid=db.Column(db.String(100), primary_key=True)
+    fileid=db.Column(db.Integer, primary_key=True)
     challengeid=db.Column(db.String(100), 
             db.ForeignKey("challenges.challengeid", ondelete="CASCADE"))
     filehash=db.Column(db.String(100))
